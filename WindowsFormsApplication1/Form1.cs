8f216b4dc515dcec5355d1aefc23eff9c3c3a21c
Astaer <wakamajenkins@mail.ru>
2018-03-21 14:14:53 +0900

    qwertyu

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -22,7 +22,7 @@ namespace WindowsFormsApplication1
             int kolichestvo = Convert.ToInt32(this.ColTarBox.Text);
             string poryadok = this.PorBox.Text;
             string Tarakan = this.ColTarBox.Text;
-
+            string Tarakan2 = this.ColTarBox.Text;
              
         }
     }
