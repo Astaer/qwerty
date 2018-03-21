4949ac3ad6f0a6de5b41cac58fff329d879f910c
Astaer <wakamajenkins@mail.ru>
2018-03-21 11:16:26 +0900

    Exam test init commit12

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColTarBox = new System.Windows.Forms.TextBox();
+            this.PorBox = new System.Windows.Forms.TextBox();
+            this.ResBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ColTarBox
+            // 
+            this.ColTarBox.Location = new System.Drawing.Point(3, 12);
+            this.ColTarBox.Name = "ColTarBox";
+            this.ColTarBox.Size = new System.Drawing.Size(114, 20);
+            this.ColTarBox.TabIndex = 0;
+            // 
+            // PorBox
+            // 
+            this.PorBox.Location = new System.Drawing.Point(3, 38);
+            this.PorBox.Name = "PorBox";
+            this.PorBox.Size = new System.Drawing.Size(114, 20);
+            this.PorBox.TabIndex = 1;
+            // 
+            // ResBox
+            // 
+            this.ResBox.Location = new System.Drawing.Point(3, 86);
+            this.ResBox.Name = "ResBox";
+            this.ResBox.Size = new System.Drawing.Size(114, 20);
+            this.ResBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Кол-во тараканов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Порядок";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Кол-во перестановок";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button1.Location = new System.Drawing.Point(3, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResBox);
+            this.Controls.Add(this.PorBox);
+            this.Controls.Add(this.ColTarBox);
+            this.Name = "Form1";
+            this.Text = "Анатолий и тараканы";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ColTarBox;
+        private System.Windows.Forms.TextBox PorBox;
+        private System.Windows.Forms.TextBox ResBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
